d375842b34c2fdfb3dbbfd858f87670bd82d0cf9
adidas820 <adaialvarez2006@gmail.com>
2024-05-09 02:04:28 +0200

    primer intento de pensar un login y jugar con los styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import Login from './Login.jsx'
+// App.js
+
+import React, { useState } from 'react';
+import Login from './Login';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState('');
+
+  const handleLogin = (user) => {
+    setUsername(user);
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setUsername('');
+    setIsLoggedIn(false);
+  };
+
   return (
-    <div className="App">
-      <>
-      <Login />
-      <Login />
-      </>
+    <div>
+      {isLoggedIn ? (
+        <div>
+          <h1>Bienvenido, {username}!</h1>
+          <button onClick={handleLogout}>Cerrar Sesión</button>
+        </div>
+      ) : (
+        <Login handleLogin={handleLogin} />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,47 @@
-function Login() {
+import React, { useState } from 'react';
+import './Login.css'; 
 
-    return (
+const Login = ({ handleLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const usr= 'adi';
+  const pwd = '123';
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes agregar la lógica para validar las credenciales
+    if (username === usr && password === pwd) {
+      handleLogin(username);
+    } else {
+      alert('Credenciales inválidas. Por favor, intenta de nuevo.');
+    }
+  };
+
+  return (
     <div>
-        <input type="text" />
+      <h2>Iniciar Sesión</h2>
+      
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Usuario: </label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Contraseña: </label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Iniciar Sesión</button>
+      </form>
     </div>
-    );
-}
+  );
+};
 
 export default Login;
